b545b9c9befd4a49a76766e660908357e1881654
Kirya <116507003+tembz@users.noreply.github.com>
2023-05-07 23:37:34 +0300

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,10 +1,15 @@
-from hella_api import HellaApi
+from hella_api import HellaApi, Handler
 from tokens import HELLA
 
 api = HellaApi(HELLA)
+api2 = Handler(HELLA)
 
+#Option 1:
 get_group = api.methods(method='getGroups', user_id=1)
+print(get_group)
 
+#Option 2:
+get_group = api2.getGroups(user_id=1)
 print(get_group)
 
 
